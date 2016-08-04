efee1d8bb0237be621963c0c76308bfcfd492a5a
Dore Chen <dore_chen@hotmail.com>
2016-08-04 22:41:22 +1200

    Can print sequence of numbers.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,17 @@
+import java.util.Arrays;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
 
-		int n = Integer.parseInt(args[0]);
-		System.out.println("Finding fibonacci of " + n);
-		System.out.println(findFib(n));
+		try {
+			int n = Integer.parseInt(args[0]);
+			long[] sequence;
+			System.out.println("The Fibonacci of " + n + " is " + findFib(n));
+			System.out.println("Sequence is " + Arrays.toString(fibSeq(n+1)));
+		} catch (NumberFormatException n) {
+			System.err.println("Invalid input. Please input a number.");
+		}
 	}
 
 	public static int findFib(int n) {
@@ -16,5 +23,16 @@ public class Fibonacci {
 		} else {
 			return findFib(n-1) + findFib(n-2);
 		}
+	}
+
+	public static long[] fibSeq(int length) {
+
+		long[] sequence = new long[length];
+		sequence[0] = 0;
+		sequence[1] = 1;
+		for (int i = 2; i < length; i++) {
+			sequence[i] = sequence[i-1] + sequence[i-2];
+		}
+		return sequence;
 	}
 }
